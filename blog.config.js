ac589ea3bb5f8e4bdab8ce3510033381e5558b34
Y2O3 <y2o3mip@gmail.com>
2024-02-09 02:35:26 +0800

    blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -150,7 +150,7 @@ const BLOG = {
   POST_LIST_PREVIEW: process.env.NEXT_PUBLIC_POST_PREVIEW || 'false', //  是否在列表加载文章预览
   POST_PREVIEW_LINES: 20, // 预览博客行数
   POST_RECOMMEND_COUNT: 7, // 推荐文章数量
-  POSTS_PER_PAGE: 12, // post counts per page
+  POSTS_PER_PAGE: 20, // post counts per page 每页数量！！！
   POSTS_SORT_BY: process.env.NEXT_PUBLIC_POST_SORT_BY || 'notion', // 排序方式 'date'按时间,'notion'由notion控制
 
   ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || null, // 在这里查看 https://dashboard.algolia.com/account/api-keys/
